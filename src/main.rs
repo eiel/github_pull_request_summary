2138500cdcf610bd97d08f78c7d024fa667058b1
Tomohiko Himura <eiel.hal@gmail.com>
2020-02-15 14:50:39 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use url::{Url};
 use github_rs::client::{Executor, Github};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
+use std::env;
 
 fn main() {
-    // TODO 環境変数から取得する
-    let token = "8c9d27c6e6e43a16c10a84d20d6fff2a3364179d";
+    let mut args = env::args();
+    args.next();
+    let url = args.next().expect("not 1st arg");
+    let token = env::var("GITHUB_API_TOKEN").expect("not env GITHUB_API_TOKEN");
     // TODO コマンドライン引数から取得
-    let url = "https://github.com/chatwork/biwa-frontend/pulls/2427";
-    let pull_request_id = parse(url).unwrap();
-    let pull = get_pull_request(token, &pull_request_id);
+    let pull_request_id = parse(&url).unwrap();
+    let pull = get_pull_request(&token, &pull_request_id);
 
     match pull {
         Ok(pull) => {
@@ -21,14 +23,14 @@ fn main() {
     }
 }
 
-fn parse(url: &str) -> Result<PullRequestID, &str> {
-    let pull_url = Url::parse(url).map_err(|_e| "url parse error")?;
-    let mut paths = pull_url.path_segments().ok_or("url pull erquest parse error")?;
+fn parse(url: &str) -> Result<PullRequestID, String> {
+    let pull_url = Url::parse(url).map_err( |_e| format!("invalid url:  {}", url))?;
+    let mut paths = pull_url.path_segments().ok_or("url pull request parse error")?;
     let owner: &str = paths.next().ok_or("non owner: pull request url")?;
     let repository: &str = paths.next().ok_or("non repository: pull request url")?;
     let pull = paths.next().ok_or("non pull path: pull request url")?;
-    if !pull.eq("pulls") {
-        return Err("non pull request url");
+    if !pull.eq("pull") {
+        return Err("non pull request url".to_owned());
     }
     let id: &str = paths.next().ok_or("non pull request id: pull request url")?;
     Ok(PullRequestID{
@@ -47,17 +49,17 @@ struct PullRequestID{
 fn text(pull: PullRequest) -> String {
     format!(
         "{} (+{},-{}) changed files {}\n{}",
-        pull.title, pull.additions, pull.deletions, pull.changed_files, pull.url
+        pull.title, pull.additions, pull.deletions, pull.changed_files, pull.html_url
     )
 }
 
 #[derive(Serialize, Deserialize)]
 struct PullRequest {
     title: String,
-    url: String,
-    additions: u8,
-    deletions: u8,
-    changed_files: u8,
+    html_url: String,
+    additions: u32,
+    deletions: u32,
+    changed_files: u32,
 }
 
 fn get_pull_request(
